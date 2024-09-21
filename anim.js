f060b62074aa3978192e2b76514e889ee3cdc345
TheVyper69 <102842192+TheVyper69@users.noreply.github.com>
2024-09-20 20:44:54 -0600

    2.0

--- anim.js
+++ anim.js
@@ -1,32 +1,7 @@
 // Sincronizar las letras con la canción
 var audio = document.querySelector("audio");
-var lyrics = document.querySelector("#lyrics");
 
-// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 
-// Animar las letras
-function updateLyrics() {
-  var time = Math.floor(audio.currentTime);
-  var currentLine = lyricsData.find(
-    (line) => time >= line.time && time < line.time + 6
-  );
-
-  if (currentLine) {
-    // Calcula la opacidad basada en el tiempo en la línea actual
-    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
-    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
-
-    // Aplica el efecto de aparición
-    lyrics.style.opacity = opacity;
-    lyrics.innerHTML = currentLine.text;
-  } else {
-    // Restablece la opacidad y el contenido si no hay una línea actual
-    lyrics.style.opacity = 0;
-    lyrics.innerHTML = "";
-  }
-}
-
-setInterval(updateLyrics, 1000);
 
 //funcion titulo
 // Función para ocultar el título después de 216 segundos
